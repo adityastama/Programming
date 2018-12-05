a8770d01b4dec4357810e556f918f0d7fbe012d4
adi tyastama <45529891+adityastama@users.noreply.github.com>
2018-12-05 10:54:45 +0700

    Create fungsi for.cpp

---
+++ fungsi for.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// FOR
+	// adalah perulangan suatu pernyataan
+
+int c;
+
+cout << "\nCONTOH PERULANGAN FOR MENINGKAT\n" << endl;
+for ( c = 1; c <= 10; c++) // angka akan bertambah sampai 10
+{
+	cout << c << endl; // kita akan keluarkan int c nya
+}
+
+return 0;
+}
