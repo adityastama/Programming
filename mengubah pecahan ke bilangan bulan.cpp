bcc18555226c830fc6c2fbc11bd9916963aa01e4
adi tyastama <45529891+adityastama@users.noreply.github.com>
2018-12-05 10:58:43 +0700

    Create mengubah pecahan ke bilangan bulan.cpp

---
+++ mengubah pecahan ke bilangan bulan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << "\nMENGUBAH PECAHAN KE BILANGAN BULAT" << endl;
+	cout << "==================================\n" << endl;
+
+	cout << "masukan angka pecahan : ";
+	cin >> a;
+
+	cout << "\nbilangan bulatnya adalah = " << a << endl;
+
+	return 0;
+}
