5c318bafa3c308e8629089f8d748af28454fb9ae
adi tyastama <45529891+adityastama@users.noreply.github.com>
2018-12-05 10:56:33 +0700

    Create fungsi while.cpp

---
+++ fungsi while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// while
+	// adalah pengulangan perintah
+	//	jika syaratnya terpenuhi / benar (true)
+
+	int a = 1;
+
+	while ( a <= 10 )
+	{
+		cout << a << endl;
+		a += 1; // ini agar loop berhenti
+				// bisa pakai assigment seperti di atas 
+	} 
+
+	cout << "selesai" << endl;
+
+	return 0;
+}
